e9ecbb142a935f3747fb9cad68a4aff0f7567d8f
Thien Huynh <49941781+neiths@users.noreply.github.com>
2025-04-28 17:26:11 +0000

    adding a route to make predictions

--- deployment-with-k8s/app.py
+++ deployment-with-k8s/app.py
@@ -1,6 +1,10 @@
 import uvicorn
 from fastapi import FastAPI, Query
 import joblib
+import numpy as np
+import pandas as pd
+import requests
+
 
 nationality_native_bayes = open("deployment-with-k8s/model/naive_bayes.pkl", "rb")
 nationality_native_cv = joblib.load(nationality_native_bayes)
@@ -10,6 +14,15 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"text": "our first route"}
+
+@app.get('/predict/{name}')
+async def predict(name: str = Query(None, min_length=2, max_length=12)):
+  if requests.method == 'GET':
+    namequery = requests.form['namequery']
+    data = [namequery]
+    vect = nationality_native_cv.transform(data).toarray()
+    result = nationality_native_cv.predict(vect)
+    return {"orig_name": name, "prediction": result}
 
 
 if __name__ == '__main__':
